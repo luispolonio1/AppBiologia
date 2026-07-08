3dd27f66a3350d72b48e8a59cc3b598f02963c43
Luis <lpoloniol@unemi.edu.ec>
2026-07-08 06:21:06 -0500

    ajustes ahora si

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -5,5 +5,10 @@ module.exports = function (api) {
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
     ],
+    plugins: [
+      // Reanimated 4 + worklets 0.8 requiere este plugin para transformar
+      // private properties de clases y workletizar funciones correctamente.
+      'react-native-worklets/plugin',
+    ],
   };
 };
